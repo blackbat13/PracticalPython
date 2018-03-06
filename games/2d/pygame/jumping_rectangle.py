30047991ec46bc1a3b192f6310d79015624455d9
blackbat <blackbat13@gmail.com>
2018-03-06 21:45:17 +0100

    Added some new content for pygame

---
+++ games/2d/pygame/jumping_rectangle.py
@@ -0,0 +1,52 @@
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+done = False
+
+clock = pygame.time.Clock()
+
+rect_x = 50
+rect_y = 50
+
+rect_change_x = 5
+rect_change_y = 5
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+
+    screen.fill(BLACK)
+
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    pygame.draw.rect(screen, RED, [rect_x + 10, rect_y + 10, 30, 30])
+
+    rect_x += rect_change_x
+    rect_y += rect_change_y
+
+    if rect_y > 450 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+    if rect_x > 650 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
