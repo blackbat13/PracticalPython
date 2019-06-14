9d79cbc5dbdb5fc25023fe551e903afdec1b17ba
blackbat13 <blackbat13@gmail.com>
2019-06-14 17:45:04 +0200

    Added cellular automata example

---
+++ other/cellular_automata.py
@@ -0,0 +1,13 @@
+# Source: https://github.com/lantunes/cellpylib
+
+import cellpylib as cpl
+
+# initialize a 60x60 2D cellular automaton
+cellular_automaton = cpl.init_simple2d(60, 60)
+
+# evolve the cellular automaton for 30 time steps,
+#  applying totalistic rule 126 to each cell with a Moore neighbourhood
+cellular_automaton = cpl.evolve2d(cellular_automaton, timesteps=30, neighbourhood='Moore',
+                                  apply_rule=lambda n, c, t: cpl.totalistic_rule(n, k=2, rule=126))
+
+cpl.plot2d(cellular_automaton)
